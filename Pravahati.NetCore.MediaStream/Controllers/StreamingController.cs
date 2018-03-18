430b6c0d1be2ef7cccd2f5d7170dd9995fc302e5
Shashank <shanky011987@gmail.com>
2018-03-18 14:52:43 +0530

    Initial Check-in for Media Streaming

---
+++ Pravahati.NetCore.MediaStream/Controllers/StreamingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pravahati.NetCore.MediaStream.Service;
+
+namespace Pravahati.NetCore.MediaStream.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[controller]")]
+    public class StreamingController : Controller
+    {
+        private IAzureVideoStreamService _streamingService;
+
+        public StreamingController(IAzureVideoStreamService streamingService)
+        {
+            _streamingService = streamingService;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<FileStreamResult> Get(string name)
+        {
+            var stream = await _streamingService.GetVideoByName(name);
+            return new FileStreamResult(stream, "video/mp4");
+        }
+    }
+}
---
+++ Pravahati.NetCore.MediaStream/Service/IAzureVideoStreamService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pravahati.NetCore.MediaStream.Service
+{
+    public interface IAzureVideoStreamService
+    {
+        Task<Stream> GetVideoByName(string name);
+    }
+}
